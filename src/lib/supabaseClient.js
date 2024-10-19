a2509b60590bf67702b523a9f7c8c9ede7359268
Ksusha <tsp.odett@gmail.com>
2024-10-19 14:08:41 +0300

    Исправление ошибки

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -7,28 +7,28 @@ const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
-
-
-async function addProduct(product) {
-    const { data, error } = await supabase
-        .from('tires')          .insert([
-            {
-                name: product.name,
-                manufacturer: product.manufacturer,
-                price: product.price,
-                image_url: product.image_url,
-                quantity: product.quantity,
-                type: product.type,
-                width: product.width,
-                height: product.height,
-                radius: product.radius,
-                season: product.season,
-                product_type: product.product_type,
-            },
-        ]);
-
-    if (error) {
-            } else {
-            }
-}
-
+//
+//
+// async function addProduct(product) {
+//     const { data, error } = await supabase
+//         .from('tires')          .insert([
+//             {
+//                 name: product.name,
+//                 manufacturer: product.manufacturer,
+//                 price: product.price,
+//                 image_url: product.image_url,
+//                 quantity: product.quantity,
+//                 type: product.type,
+//                 width: product.width,
+//                 height: product.height,
+//                 radius: product.radius,
+//                 season: product.season,
+//                 product_type: product.product_type,
+//             },
+//         ]);
+//
+//     if (error) {
+//             } else {
+//             }
+// }
+//
